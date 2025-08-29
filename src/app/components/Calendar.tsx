366c9fbea45cc7bdd0c19fa4fb654edb1f2ede83
Rachel <urakhel7@gmail.com>
2025-08-29 15:33:18 +0200

    Creating calendar component with main logic.

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Calendar = () => {
+    //The day of the week array in short form
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+   
+    //Capturing today's date and all the good stuff
+    const today = new Date();
+    const month = today.getMonth() //In js months are 0 indexed & day 0 is the last day of the previous month
+    const year = today.getFullYear()
+    //Most neat way but I could have used an array with actual days in months [31, 28, etc...] the 
+    const monthDays = new Date(year, month +1, 0).getDate(); //year of interest:today, next month, last day of the current month
+    const monthFirstDay = new Date(year, month, 1).getDay();
+
+    //Capturing all days in an array (due to typescripts strictness I had to add the type annotation to explain the array c nbrs/nulls)
+    const days: (number | null)[] = Array.from({ length: monthFirstDay }, () => null); // Fill with nulls the first few days
+    for (let i = 1; i <= monthDays; i++) {
+        days.push(i);
+    }
+
+    return (
+        <div className="p-4">
+            <div className="text-center text-xl font-bold mb-4">
+                {new Date(year, month).toLocaleString('default', {month: 'long', year: 'numeric'})}
+            </div>
+            <div className="grid grid-cols-7 gap-1 text-center font-bold text-gray-600">
+                {weekDays.map(day => (
+                    <div key={day} className="py-2">
+                        {day}
+                    </div>
+                ))}
+            </div>
+            <div className="grid grid-cols-7 gap-1">
+                {days.map((day, index) => (
+                    <div key={index} 
+                    className={`p-4 border rounded-lg cursor-pointer ${
+                    day ? 'border-gray-200 hover:bg-gray-100' : 'border-transparent'
+                    }`}>
+                        {day}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Calendar;
